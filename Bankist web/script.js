c7a632df18a0cc453d4f5408d2dd14c0f88b980c
mponpol <mponpol@gmail.com>
2022-03-24 22:30:54 +0100

    implement smooth scroll on webpage

--- Bankist web/script.js
+++ Bankist web/script.js
@@ -8,7 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  // 'Open account' isn't a button but a link, so to avoid the page to scroll up once we click on it we need to fix that manually with the 'preventDefault' method
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +20,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+// Refactorized up
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +32,29 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // Scrolling
+
+  // Old way:
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX, // pageXOffset is deprecated; use scrollX
+  //   s1coords.top + window.scrollY // pageYOffset is deprecated; use scrollY
+  // );
+  // Old way (improved):
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  // New way:
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
