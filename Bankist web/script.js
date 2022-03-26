5eb1d41acd990379d9924bf223d083895177a345
mponpol <mponpol@gmail.com>
2022-03-26 19:38:54 +0100

    implement revealing elements on scroll

--- Bankist web/script.js
+++ Bankist web/script.js
@@ -141,18 +141,6 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // });
 
 // using Intersection Observer API
-// const obsCallback = function (entries, observer) {
-//   entries.forEach(entry => {});
-// };
-
-// const obsOptions = {
-//   root: null,
-//   threshold: 0.1,
-// };
-
-// const observer = new IntersectionObserver(obsCallback, obsOptions);
-// observer.observe();
-
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 
@@ -171,3 +159,26 @@ const stickyOptions = {
 
 const headerObserver = new IntersectionObserver(stickyNav, stickyOptions);
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
